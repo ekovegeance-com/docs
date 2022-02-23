e487ae7ca10c636d0dc1f1284b44186da1fe7301
ekovegeance <me@ekovegeance.com>
2022-02-23 07:32:43 -0500

    Update Tugas OOP Pert1

---
+++ Semester 4/Pemrograman Berorientasi Objek/Pert1/mahasiswa/Program.cs
@@ -0,0 +1,36 @@
+﻿// Program OOP - C#
+
+
+//Class Program
+internal class Program{
+    //Main Method
+    public static void Main(string[] args){
+        //Object dari Class Mahasiswa
+        mahasiswa mhs = new mahasiswa();
+        mhs.nama = "Eko";
+        mhs.nim = "201420001";
+        mhs.alamat = "Jl. Binawaluya";
+        mhs.matkul = "Object Oriented Programming";
+        mhs.hari = "Selasa - Rabu";
+        mhs.dataMhs();
+    }
+}
+
+// Class mahasiswa
+public class mahasiswa {
+    //Atribut
+    public string nama;
+    public string nim;
+    public string alamat;
+    public string hari;
+    public string matkul;
+
+    //Method
+    public void dataMhs(){
+        Console.WriteLine("Nama : "+ nama);
+        Console.WriteLine("NIM : "+ nim);
+        Console.WriteLine("Alamat : "+ alamat);
+        Console.WriteLine("Sedang belajar : "+ matkul +" "+ "pada hari :"+ hari);
+        Console.ReadKey();
+    }
+}
